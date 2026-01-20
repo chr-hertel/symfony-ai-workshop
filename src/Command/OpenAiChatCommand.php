4d4d0b5272532b367fba60b2fdb860e83574cbe7
Christopher Hertel <mail@christopher-hertel.de>
2026-01-20 11:43:28 +0100

    Adopting Platform component in cmd

--- src/Command/OpenAiChatCommand.php
+++ src/Command/OpenAiChatCommand.php
@@ -2,19 +2,17 @@
 
 namespace App\Command;
 
+use Symfony\AI\Platform\Message\Message;
+use Symfony\AI\Platform\Message\MessageBag;
+use Symfony\AI\Platform\PlatformInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsCommand('app:openai:chat', 'Chat with GPT')]
 final readonly class OpenAiChatCommand
 {
     public function __construct(
-        private HttpClientInterface $httpClient,
-        #[\SensitiveParameter]
-        #[Autowire('%env(OPENAI_API_KEY)%')]
-        private string $apiKey,
+        private PlatformInterface $platform,
     ) {
     }
 
@@ -22,23 +20,14 @@ final readonly class OpenAiChatCommand
     {
         $message = $io->ask('What is the message?');
 
-        $response = $this->httpClient->request('POST', 'https://api.openai.com/v1/chat/completions', [
-            'headers' => ['Content-Type' => 'application/json'],
-            'auth_bearer' => $this->apiKey,
-            'json' => [
-                'model' => 'gpt-4o',
-                'temperature' => 1.0,
-                'messages' => [
-                    ['role' => 'system', 'content' => 'You are a pirate and you write funny.'],
-                    ['role' => 'user', 'content' => $message],
-                ],
-            ],
-        ]);
+        $messageBag = new MessageBag(
+            Message::forSystem('You are a pirate and you write funny.'),
+            Message::ofUser($message),
+        );
 
+        $response = $this->platform->invoke('gpt-4o-mini', $messageBag);
 
-        $assistantMessage = $response->toArray()['choices'][0]['message']['content'];
-
-        $io->block($assistantMessage);
+        $io->block($response->asText());
 
         return 0;
     }
